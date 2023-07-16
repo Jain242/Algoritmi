3177f9faf805a84cb595834709612a0bf8604e55
Egor <agent-nino@bk.ru>
2023-07-16 13:55:21 +0300

    HW2

---
+++ HW2/LinkedList.java
@@ -0,0 +1,79 @@
+package HW2;
+
+import org.w3c.dom.Node;
+
+
+public class LinkedList {
+    private Node head;
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void reverse() {
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        head = previous;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    //@Overvide
+    // public String toString(){
+
+
+
+    // }
+
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println("Исходный список:");
+        list.display();
+
+        System.out.println("Разворот списка:");
+        list.reverse();
+        list.display();
+    }
+}
